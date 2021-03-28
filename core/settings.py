76be5069dbd8a6f63c37b22805e3545d2719a3c2
Eduardo Cristiano Thums <eduardocristiano01@gmail.com>
2021-03-27 21:34:16 -0300

    chore: create docker configuration of needed services

--- core/settings.py
+++ core/settings.py
@@ -49,8 +49,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'authentication-poc',
+        'USER': 'authentication-poc',
+        'PASSWORD': 'authentication-poc',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
